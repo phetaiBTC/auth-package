441e8f2a2c24de669ab97dc6735e6176e0ab70f0
phetaibtc <phetaibtc@gmail.com>
2026-03-08 13:12:36 +0700

    oo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ export * from './auth/auth.service'
 export * from './auth/guard/auth.guard'
 export * from './auth/decorator/auth.decorator'
 export * from './auth/decorator/public.decorator'
-export * from './auth/strategy/jwt.strategy'
-export * from './auth/interfaces/jwt-payload.interface'
-
-
